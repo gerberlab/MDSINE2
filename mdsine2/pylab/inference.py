1361c77756fa16fd38e7dc869e657b9d02bea39b
Younhun Kim <younhun@mit.edu>
2021-03-04 10:39:54 -0500

    Added intermediate iteration printing in debug.

--- mdsine2/pylab/inference.py
+++ mdsine2/pylab/inference.py
@@ -435,6 +435,7 @@ class BaseMCMC(BaseModel):
             intermediate_time = time.time()
 
             for i in tqdm(range(self.start_step, self.n_samples), file=sys.stdout):
+                logger.debug("Iteration {} / {}".format(i+1, self.n_samples))
                 self.sample_iter = i
 
                 # Check if we need to run the intermediate script
