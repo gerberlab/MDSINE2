e17b34bf6595348024a24bf26c7ff1572f758a73
Younhun Kim <younhun@mit.edu>
2021-07-08 17:08:13 -0400

    Fixed version numbering. Cleaned up setup script.

--- setup.py
+++ setup.py
@@ -7,25 +7,13 @@ For installing on Windows OS
 For python 3.7.3, `distutils` has a bug that is described issue 35893 in Python Bug
 Tracker (https://bugs.python.org/issue35893). This bug does not affect MacOS.
 '''
-import os
+from pathlib import Path
+from setuptools import setup
+from distutils.core import Extension
 
-from setuptools import setup, find_packages
-from distutils.core import Extension #, setup
+lib_dir = Path(__file__).resolve().parent
 
-# This is for it to run on windows
-if os.name == 'nt':
-    from distutils.command import build_ext
-    def get_export_symbols(self, ext):
-        parts = ext.name.split(".")
-        print('parts', parts)
-        if parts[-1] == "__init__":
-            initfunc_name = "PyInit_" + parts[-2]
-        else:
-            initfunc_name = "PyInit_" + parts[-1]
-
-    build_ext.build_ext.get_export_symbols = get_export_symbols
-
-VERSION = '4.0.7'
+VERSION = '0.1.0'
 SHORT_DESC = 'Implements core features of the MDSINE2 model'
 LONG_DESC = \
     '''
@@ -48,45 +36,39 @@ LONG_DESC = \
     Somethings this package does not do well:
         - Optimization based algorithms (use PyTorch or TensorFlow)
     '''
-REQUIREMENTS = [
-    'numpy>=1.19.4',
-    'pandas>=1.1.4',
-    'matplotlib==3.3.1',
-    'sklearn==0.0',
-    'seaborn==0.11.0',
-    'h5py==2.9.0',
-    'psutil==5.7.3',
-    'ete3==3.1.2',
-    # 'PySide2==5.15.2',
-    # 'PyQt5', 
-    'networkx==2.3',
-    'numba==0.52',
-    'biopython==1.78',
-    'treeswift==1.1.14',
-    'py2cytoscape==0.7.1']
+
+# Package requirements: Parse from `requirements.txt`.
+requirementPath = lib_dir / 'requirements.txt'
+REQUIREMENTS = []
+if Path(requirementPath).is_file():
+    with open(requirementPath, "r") as f:
+        REQUIREMENTS = f.read().splitlines()
 
 # Custom C distributions
-ext1 = Extension('_distribution', ['mdsine2/pylab/c_code/distributionmodule.c'])
-ext2 = Extension('_sample', ['mdsine2/pylab/c_code/_samplemodule.c'])
-EXTENSIONS = [ext1, ext2]
+EXTENSIONS = [
+    Extension('_distribution', ['mdsine2/pylab/c_code/distributionmodule.c']),
+    Extension('_sample', ['mdsine2/pylab/c_code/_samplemodule.c'])
+]
 
 # Subpackages
 PACKAGES = [
     'mdsine2',
     'mdsine2.pylab',
-    'mdsine2.initializers']
+    'mdsine2.initializers'
+]
 
 setup(
     name='mdsine2',
     version=VERSION,
     description=SHORT_DESC,
     long_description=LONG_DESC,
-    author='David Kaplan',
-    author_email='dkaplan65@gmail.com',
-    python_requires='>=3.7',
+    author='David Kaplan, Travis Gibson, Younhun Kim, Sawal Acharya',
+    author_email='tegibson@bwh.harvard.edu',
+    python_requires='>=3.7.3',
     license='GNU',
     packages=PACKAGES,
     zip_safe=False,
     install_requires=REQUIREMENTS,
     ext_modules=EXTENSIONS,
-    include_package_data=True)
+    include_package_data=True
+)
